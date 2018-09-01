b1aae650ccb17354201fe76def88e9e358f61760
kris <kris>
2018-09-01 19:17:46 +0200

    install symfony 2.8.6

--- website/web/app.php
+++ website/web/app.php
@@ -2,7 +2,10 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
-require __DIR__.'/../app/autoload.php';
+/**
+ * @var Composer\Autoload\ClassLoader
+ */
+$loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../app/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
